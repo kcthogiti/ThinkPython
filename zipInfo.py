4afeabf504f1db916fb8d9fd86c6ba795cacc507
Krishna Chaitanya Thogiti <kc.thogiti@gmail.com>
2015-11-15 10:49:27 -0600

    test
    test

--- zipInfo.py
+++ zipInfo.py
@@ -9,3 +9,6 @@ else:
 
 webbrowser.open("https://www.google.com/maps/place/" + address)
 webbrowser.open_new_tab("http://www.weather.com/weather/today/l/" + address + ":4:US")
+
+
+# adding comments to test github
